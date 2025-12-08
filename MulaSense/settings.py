5d6c11372cd8b8e049f4817f553ee8c86153bdba
Eng-Chiremba <cchiremba@gmail.com>
2025-12-08 16:12:11 +0200

    modified:   MulaSense/settings.py

--- MulaSense/settings.py
+++ MulaSense/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&z0nzf0hy^6uk1!vh7hr743aiz4b)2&s*k4l9_869$zldl(^!)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,mulasense.onrender.com').split(',') 
 
@@ -91,7 +91,11 @@ WSGI_APPLICATION = 'MulaSense.wsgi.application'
 
 if os.environ.get('DATABASE_URL'):
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(
+            os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
