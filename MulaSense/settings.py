0d65e4c443cc22f9ac5a326647cf82f4dbab5463
Eng-Chiremba <cchiremba@gmail.com>
2025-12-08 15:39:30 +0200

    modified:   MulaSense/settings.py

--- MulaSense/settings.py
+++ MulaSense/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&z0nzf0hy^6uk1!vh7hr7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,mulasense.onrender.com').split(',')
 
 
 # Application definition
